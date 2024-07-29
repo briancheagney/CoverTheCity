3b6ab2ffc12b4e6991da244a2dad3f6f441dd745
Brian Heagney <bheagney@highpoint.edu>
2024-07-29 15:17:41 -0400

    Improved Start screen / added reset character button
    Added high point to the start screen to jazz it up a bit. Revised character select so that the buttons can be individualized easier, and added the ability to reset the buttons. Also, character select buttons function differently. Clicking the first woman automatically unlocks the man for the next round. clicking the man automatically unlocks the dog. I wrote a new script for it. And duplicated the original scene, so it looks like there's two character select scenes now.

---
+++ Assets/Scripts/HeagneysScripts/Heag_CharSelecter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StorySelectionManager : MonoBehaviour
+{
+    public Button story1Button;
+    public Button story2Button;
+    public Button story3Button;
+
+    void Start()
+    {
+        // Load the completion states
+        bool isStory1Completed = PlayerPrefs.GetInt("Story1Completed", 0) == 1;
+        bool isStory2Completed = PlayerPrefs.GetInt("Story2Completed", 0) == 1;
+        bool isStory3Completed = PlayerPrefs.GetInt("Story3Completed", 0) == 1;
+
+        // Enable/Disable buttons based on completion states
+        story1Button.interactable = true;  // Story1 button is always enabled
+        story2Button.interactable = isStory1Completed;  // Enable Story2 button if Story1 is completed
+        story3Button.interactable = isStory2Completed;  // Enable Story3 button if Story2 is completed
+
+        // Add listeners for buttons
+        story1Button.onClick.AddListener(() => PlayStory(1));
+        story2Button.onClick.AddListener(() => PlayStory(2));
+        story3Button.onClick.AddListener(() => PlayStory(3));
+    }
+
+    void PlayStory(int storyNumber)
+    {
+        // Load the story scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Story" + storyNumber);
+
+        // Mark the story as completed
+        PlayerPrefs.SetInt("Story" + storyNumber + "Completed", 1);
+        PlayerPrefs.Save();
+    }
+    public void ResetProgress()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
