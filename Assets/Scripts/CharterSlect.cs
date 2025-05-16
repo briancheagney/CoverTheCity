965dbeebefd1605fe90d43584ff7127e93e9b2ed
Brian Heagney <bheagney@highpoint.edu>
2025-05-16 11:22:03 -0400

    Character Select - set them to switch to happier images after play

--- Assets/Scripts/CharterSlect.cs
+++ Assets/Scripts/CharterSlect.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class CharterSlect : MonoBehaviour
 {
     public Button[] buttons;
+    public Sprite[] happySprites;         // 3 happy face sprites (same order as buttons)
+    public Sprite[] sadSprites;           // 3 sad face sprites (same order as buttons)
     public Sprite clickedSprite; //new sprite to switch button to
 
     private void Awake()
@@ -14,20 +16,39 @@ public class CharterSlect : MonoBehaviour
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
         for (int i = 0; i < buttons.Length; i++)
         {
-            buttons[i].interactable = false;
-        }
-        for (int i = 0; i < unlockedLevel; i++)
-        {
-            buttons[i].interactable = true;
+            bool isUnlocked = i < unlockedLevel;
+            bool isComplete = PlayerPrefs.GetInt("Person" + (i + 1) + "Complete", 0) == 1;
+
+            buttons[i].interactable = isUnlocked;
+            buttons[i].image.sprite = isComplete ? happySprites[i] : sadSprites[i];
         }
     }
     public void OpenLevel(int levelId)
     {
+        // Find which button was clicked
         Button clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
-        if (clickedButton != null && clickedSprite != null)
+
+        // Find the index of the clicked button in your buttons array
+        int clickedIndex = -1;
+        for (int i = 0; i < buttons.Length; i++)
         {
-            clickedButton.image.sprite = clickedSprite;
+            if (buttons[i] == clickedButton)
+            {
+                clickedIndex = i;
+                break;
+            }
         }
+
+        if (clickedIndex != -1)
+        {
+            PlayerPrefs.SetInt("Person" + (clickedIndex + 1) + "Complete", 1);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.LogWarning("Clicked button not found in buttons array");
+        }
+
         SceneManager.LoadScene(levelId);
     }
 }
